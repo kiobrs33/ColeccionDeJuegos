f319819d09b086bba4aceaacdb39aa1dd30b8931
kiobrs33 <rene.lozano@tecsup.edu.pe>
2019-04-25 14:55:36 -0500

    Initial Commit

---
+++ ColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColeccionDeJuegos
+//
+//  Created by MAC18 on 25/04/19.
+//  Copyright © 2019 MAC18. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
